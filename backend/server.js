14c09a5abc39dc7646a07e17f4764de4eb1a7106
Sharad <sharad2014t@gmail.com>
2025-11-01 23:52:53 +0530

    .gitignore updated

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 const app=require('./src/app');
 const connectDB=require('./src/db/db');
+const dotenv = require('dotenv');
+dotenv.config();
+
 
 connectDB();
 
--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 function connectDB(){
-    mongoose.connect("mongodb://localhost:27017/reelbite")
+    mongoose.connect(process.env.MONGO_URL)
     .then(()=>{
         console.log("Connected to MongoDB");
     })
