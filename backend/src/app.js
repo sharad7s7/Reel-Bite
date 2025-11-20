408033e91657a09b6d99a66e693e276a2c67c2e2
Sharad <sharad2014t@gmail.com>
2025-11-20 22:54:12 +0530

    env file updated

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     credentials: true
 }));
 app.use(cookieParser());
