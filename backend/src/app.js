6705753dd16b9b4eb8a1d88b5a6b3e0eced52440
Sharad <sharad2014t@gmail.com>
2025-11-20 23:17:43 +0530

    render deployment changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "http://localhost:5173",
     credentials: true
 }));
 app.use(cookieParser());
